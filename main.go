987d8d93dc2de521d749ba790232072d9b696cea
Hassani sayed <hassani.sayedgmail.com>
2022-06-20 15:50:09 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println(quote.Glass())
+	var userName string
+	fmt.Println("hello sayed")
+	fmt.Scan(&userName)
+	fmt.Printf("This is  %v'code", userName)
+
+}
