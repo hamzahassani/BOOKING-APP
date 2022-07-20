46a342b995f6802b07f653c5d22fc3702deef9e4
Hassani sayed <hassani.sayedgmail.com>
2022-07-20 10:58:57 +0100

    fix 33

--- main.go
+++ main.go
@@ -4,11 +4,20 @@ import "fmt"
 
 func main() {
 
+	// slice
+
 	var notes []int
 	notes = append(notes, 1, 2, 3)
 	for _, v := range notes {
 		println(v)
 	}
+
+	// map
+	var scores map[string]int
+	fmt.Println(scores == nil)
+
+	totalWins := map[string]int{}
+	fmt.Println(totalWins == nil)
 
 	m := map[string]int{
 		"hello": 3,
@@ -18,8 +27,30 @@ func main() {
 	v, ok := m["test"]
 	fmt.Println(v, ok)
 	for k, v := range m {
-		println(k, v)
-		println(k, v)
+		fmt.Println(k, v)
+		fmt.Println(k, v)
 	}
+
+	// struct
+	type person struct {
+		name string
+		age  int
+		pet  string
+	}
+
+	var fred person
+	fmt.Println((fred))
+	//A struct literal can be assigned to a variable as well:
+	bob := person{"sayed", 42, "jaw"}
+	fmt.Println((bob))
+	beth := person{
+		age:  30,
+		name: "Beth",
+	}
+	fmt.Println((beth))
+
+	sayed := person{}
+	sayed.name = "sayed"
+	fmt.Println((sayed))
 
 }
